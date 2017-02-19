09071a500bf6135dd3e9b5d6024104e7c81ba284
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2017-02-20 02:14:40 +0500

    Updated control code

--- main.cpp
+++ main.cpp
@@ -19,7 +19,12 @@
 
 #include <QCoreApplication>
 #include <iostream>
-#include <queue>
+#include <wiringPi.h>
+#include <softTone.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 #include "timer.h"
 
 using namespace std;
@@ -28,26 +33,37 @@ using namespace std;
 #define TOLERANCE 2.0 // 2 degrees F
 #endif
 
-void TurnOnFan() {
+#define RELAY_PIN 14 // the 5v relay module is on this GPIO pin, it controls the fan
+#define DHTPIN 16 // the pin for the temp/humidity sensor
+#define MAXTIMINGS 85
+#define MAXHISTORICAL 14
 
+bool _fanOn = false;
+
+void TurnOnFan() {
+    digitalWrite(RELAY_PIN, LOW);
+    cout << "Humidifier fan on" << endl;
+    _fanOn = true;
 }
 
 void TurnOffFan() {
-
+    digitalWrite(RELAY_PIN, HIGH);
+    cout << "Humidifier fan off" << endl;
+    _fanOn = false;
 }
 
-double GetAverage(double readings[7]) {
+double GetAverage(double readings[MAXHISTORICAL]) {
     double avg = 0.0;
 
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < MAXHISTORICAL; i++) {
         avg = avg + readings[i];
     }
 
-    return avg / 7;
+    return avg / MAXHISTORICAL;
 }
 
-void ShiftReadings(double readings[7]) {
-    for (int i = 1; i < 7; i++) {
+void ShiftReadings(double readings[MAXHISTORICAL]) {
+    for (int i = 1; i < MAXHISTORICAL; i++) {
         readings[i - 1] = readings[i];
     }
 }
@@ -56,12 +72,67 @@ double GetHumiditySettingFromUser() {
     return 50;
 }
 
-double GetCurrentTemperature() {
-    return 72.5;
-}
+int dht11_dat[5] = { 0, 0, 0, 0, 0 };
 
-double GetCurrentHumidity() {
-    return 50;
+bool GetCurrentReadings(double &temp, double &humidity) {
+
+    uint8_t laststate	= HIGH;
+    uint8_t counter		= 0;
+    uint8_t j		= 0, i;
+    float	f;
+
+    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+    // pull pin down for 18 milliseconds
+        pinMode(DHTPIN, OUTPUT);
+        digitalWrite(DHTPIN, LOW);
+        delay(18);
+        // then pull it up for 40 microseconds
+        digitalWrite(DHTPIN, HIGH);
+        delayMicroseconds(40);
+        // prepare to read the pin
+        pinMode(DHTPIN, INPUT);
+
+    //reads %RH and Temp. (F & C)
+    for ( i=0; i< MAXTIMINGS; i++) {
+            counter = 0;
+            while (digitalRead(DHTPIN) == laststate) {
+                counter++;
+                delayMicroseconds(1);
+                if (counter == 255) {
+                    break;
+                }
+            }
+            laststate = digitalRead(DHTPIN);
+
+            if (counter == 255) break;
+
+            // ignore first 3 transitions
+            if ((i >= 4) && (i%2 == 0)) {
+                // shove each bit into the storage bytes
+                dht11_dat[j/8] <<= 1;
+                if (counter > 16)
+                    dht11_dat[j/8] |= 1;
+                j++;
+            }
+        }
+
+    if ((j >= 40) &&
+                (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)) ) {
+            f = dht11_dat[2] * 9. / 5. + 32;
+
+        printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
+            dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f );
+
+        temp = f;
+        humidity = dht11_dat[0];
+        return true;
+    }
+    else
+    {
+        printf( "Bad data read from DHT11, skipping\n" );
+        return false;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -70,53 +141,93 @@ int main(int argc, char *argv[])
 
     cout << "Initializing..." << endl;
 
-    Timer t = Timer(5000); // wait 5 sec between readings
-
-    t.Start();
-
-    double pastTempReadings [7];
-    double pastHumidityReadings [7];
-
-    double curTemp = GetCurrentTemperature();
-    double curHumidity = GetCurrentHumidity();
-
-    for (int i = 0; i < 7; i++) {
-        pastTempReadings[i] = curTemp;
-        pastHumidityReadings[i] = curHumidity;
+    if (wiringPiSetupGpio() == -1) {
+        cout << "ERROR: WiringPi Setup Gpio failed!" << endl;
+        exit(1);
     }
 
+    cout << "Wiring Pi setup completed" << endl;
+
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, HIGH); // fan starts in OFF position!
+
+    cout << "Relay PIN set" << endl;
+
+    double pastTempReadings [MAXHISTORICAL];
+    double pastHumidityReadings [MAXHISTORICAL];
+
+    double curTemp = 0.0;
+    double curHumidity = 0.0;
+
+    cout << "Getting baseline temperature and humidity readings..." << endl;
+    while (true)
+    {
+        if (GetCurrentReadings(curTemp, curHumidity) == true)
+        {
+            for (int i = 0; i < MAXHISTORICAL; i++) {
+                pastTempReadings[i] = curTemp;
+                pastHumidityReadings[i] = curHumidity;
+            }
+            break;
+        }
+        delay(2000);
+    }
+    cout << "Baseline temperature and humidity set" << endl;
+
     double setHumidity = 0.0;
+
+    cout << "Starting timer..." << endl;
+    Timer timer = Timer(2000);
+    timer.Start();
+    cout << "Timer set" << endl;
+
+    cout << "Running control code..." << endl;
 
     while (true) {
 
         // update potentiometer outside of timer loop. Loop is only for measurements!
         setHumidity = GetHumiditySettingFromUser();
 
-        if (t.GetIsElapsed() == true) {
-            // timer elapsed, so let's do a readings check and then reset the timer
+        delay(100);
 
-            ShiftReadings(pastHumidityReadings);
-            ShiftReadings(pastTempReadings);
+        if (timer.GetIsElapsed() == true)
+        {
+            bool valid = GetCurrentReadings(curTemp, curHumidity);
 
-            curHumidity = GetCurrentHumidity();
-            curTemp = GetCurrentTemperature();
+            delay(20);
 
-            pastHumidityReadings[7] = curHumidity;
-            pastTempReadings[7] = curTemp;
+            if (valid) {
+                ShiftReadings(pastHumidityReadings);
+                ShiftReadings(pastTempReadings);
+
+                pastHumidityReadings[MAXHISTORICAL - 1] = curHumidity;
+                pastTempReadings[MAXHISTORICAL - 1] = curTemp;
+            }
 
             auto avgHumdity = GetAverage(pastHumidityReadings);
             auto avgTemp = GetAverage(pastTempReadings);
 
-            // If the running average over past 7 readings is less than the SET value minus the allowable TOLERANCE, then turn on the fan
-            if (avgHumdity < setHumidity - TOLERANCE) {
-                TurnOnFan();
+            // TODO display avg humidity
+            // TODO display avg temp
+
+            // If the running average over past MAHISTORICAL readings is less than the SET value minus the allowable TOLERANCE, then turn on the fan
+            if (avgHumdity < setHumidity - TOLERANCE)
+            {
+                if (_fanOn == false)
+                {
+                    TurnOnFan();
+                }
             }
-            else {
-                TurnOffFan();
+            else
+            {
+                if (_fanOn == true)
+                {
+                    TurnOffFan();
+                }
             }
 
-            t.Reset();
-            t.Start();
+            timer = Timer(2000);
+            timer.Start();
         }
     }
 
